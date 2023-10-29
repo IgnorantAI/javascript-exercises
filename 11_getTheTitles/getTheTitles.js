e385c38a10ac048f636ea1c72c328e2c46beb1d1
IgnorantAI <slave93.g@gmail.com>
2023-10-29 09:51:08 +0100

    one more

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles =  function(array) {
+    let list = array.map(e => e.title)
+    return list;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
