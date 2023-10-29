a0d57a763daa9deac5da0aa734706f852be3a64d
IgnorantAI <slave93.g@gmail.com>
2023-10-29 09:50:35 +0100

    that was 'fun'

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldest = {};
+    let currentAge = 0;
+    let currentDate = new Date().getFullYear();
 
-};
+    array.forEach(element => {     
+
+        element.yearOfDeath = element?.yearOfDeath ?? currentDate;
+    
+        if ((element.yearOfDeath - element.yearOfBirth) > currentAge )
+        {
+            oldest =  element;
+            currentAge = element.yearOfDeath - element.yearOfBirth;
+        }
+        })
+    return  oldest;
+}
+
+/*
+1. find the oldest even if dead (yearOfDeath - yearOfBirth > age)
+2. check if someone is still living => return oldest (yearOfDeath - yearOfBirth > age)
+3. returns oldest IF oldest.yearOfDeath === undefined;
+*/
 
 // Do not edit below this line
 module.exports = findTheOldest;
